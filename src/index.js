33397566af1d563fbf17214ee9905f786f57d438
MukhamedovK <kamoliddin2619@gmail.com>
2025-01-10 19:45:34 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+
+
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => {
+  console.log(`PORT: ${PORT}`)
+})
+
+
+
